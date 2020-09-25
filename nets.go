3569e00df80cdf213c0dd188b520b047bf28da2b
Jeffrey Stiles <freb@users.noreply.github.com>
2020-09-24 20:45:43 -0700

    cleanups and tests

--- nets.go
+++ nets.go
@@ -7,26 +7,22 @@ import (
 	"net"
 )
 
-// sources:
-// https://github.com/freb/netaddr/blob/master/netaddr/ip.go
-// https://github.com/docker/libnetwork/blob/master/netutils/utils.go
-
-// I need to rewrite all the functions I want to use
-// I should also check the speed of IPAdd against the code by russ cox that everyone uses:
+// IPInc increments the ip, updating the value in place. This method should be
+// used when all IP address values in a range are needed. To only get the final
+// value after x increments, use IPAdd instead as it is much faster.
 //
-//  http://play.golang.org/p/m8TNTtygK0
-// func inc(ip net.IP) {
-// 	for j := len(ip) - 1; j >= 0; j-- {
-// 		ip[j]++
-// 		if ip[j] > 0 {
-// 			break
-// 		}
-// 	}
-// }
-
-// if it is faster, just use it, and don't worry about all this guys functions.
-// otherwise, use these, because they make more sense, but add descriptions and remove functions that
-// i dont need like the check on ipv4/ipv6
+// Be careful when calling with an IPv4 address in 16-byte form, such as that
+// returned by net.IPv4() as callin git on 255.255.255.255 will produce
+// unexpected results. Call on a 4-byte IPv4 address or an IPv6 address will
+// predicably roll over to 0.0.0.0 or its equivalent.
+func IPInc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
 
 func isZeros(p net.IP) bool {
 	for _, b := range p {
@@ -43,21 +39,25 @@ func IsIPv4(ip net.IP) bool {
 		isZeros(ip[0:10]) && ip[10] == 0xff && ip[11] == 0xff
 }
 
+// IPToI32 converts a net.IP to an int32.
 func IPToI32(ip net.IP) int32 {
 	ip = ip.To4()
 	return int32(ip[0])<<24 | int32(ip[1])<<16 | int32(ip[2])<<8 | int32(ip[3])
 }
 
+// I32ToIP converts an int32 to a net.IP.
 func I32ToIP(a int32) net.IP {
 	return net.IPv4(byte(a>>24), byte(a>>16), byte(a>>8), byte(a))
 }
 
+// IPToU64 converts a net.IP to a uint64.
 func IPToU64(ip net.IP) uint64 {
 	return uint64(ip[0])<<56 | uint64(ip[1])<<48 | uint64(ip[2])<<40 |
 		uint64(ip[3])<<32 | uint64(ip[4])<<24 | uint64(ip[5])<<16 |
 		uint64(ip[6])<<8 | uint64(ip[7])
 }
 
+// u64ToIP converts a uint64 to a net.IP.
 func u64ToIP(ip net.IP, a uint64) {
 	ip[0] = byte(a >> 56)
 	ip[1] = byte(a >> 48)
@@ -69,7 +69,10 @@ func u64ToIP(ip net.IP, a uint64) {
 	ip[7] = byte(a)
 }
 
-// IPAdd adds offset to ip
+// IPAdd increments the offset amount. A new IP address is returned. Use this
+// function when only the resulting IP address is nedded. If intermediate values
+// are needed, such as when calling with offset=1 in a loop, use IPInc instead
+// as it is much faster.
 func IPAdd(ip net.IP, offset int) net.IP {
 	if IsIPv4(ip) {
 		a := int(IPToI32(ip[len(ip)-4:]))
@@ -91,9 +94,6 @@ func IPAdd(ip net.IP, offset int) net.IP {
 	return ip
 }
 
-// this would help the a UsableNetRange func
-// func IPSub
-
 // IPMod calculates ip % d
 func IPMod(ip net.IP, d uint) uint {
 	if IsIPv4(ip) {
@@ -105,54 +105,44 @@ func IPMod(ip net.IP, d uint) uint {
 	return uint(((hi%b)*((0-b)%b) + lo%b) % b)
 }
 
-// my function
 // IPMaskCount returns the number of addresses in a subnet with that mask
 func IPMaskCount(m net.IPMask) int {
 	ones, bits := m.Size()
 	return 1 << (uint(bits - ones))
 }
 
-// my function (not really but its ok, simple and renamed)
-// slices point to an underlying array, so creating a new variable alone will not create a copy
-func CopyIP(from net.IP) net.IP {
-	if from == nil {
-		return nil
+// CopyIP makes a newly allocated copy of the provided ip address.
+func CopyIP(ip net.IP) net.IP {
+	if ip == nil {
+		ip = net.IPv4zero
 	}
-	to := make(net.IP, len(from))
-	copy(to, from)
-	return to
+	dst := make(net.IP, len(ip))
+	copy(dst, ip)
+	return dst
 }
 
-func NetsOverlap(netX *net.IPNet, netY *net.IPNet) bool {
-	return netX.Contains(netY.IP) || netY.Contains(netX.IP)
+// NetsOverlap returns true if the two networks have overlapping ip addresses.
+func NetsOverlap(netA net.IPNet, netB net.IPNet) bool {
+	return netA.Contains(netB.IP) || netB.Contains(netA.IP)
 }
 
-// changed name, docker function
-// // NetRange
-func NetRange(network *net.IPNet) (net.IP, net.IP) {
-	if network == nil {
-		return nil, nil
+// NetRange returns the first (network) and last (broadcast) IP addresses from
+// a net.IPNet
+func NetRange(ipnet net.IPNet) (net.IP, net.IP) {
+	first := ipnet.IP.Mask(ipnet.Mask)
+	last := make(net.IP, len(first))
+	for i, b := range first {
+		last[i] = b | ^ipnet.Mask[i]
 	}
 
-	firstIP := network.IP.Mask(network.Mask)
-	lastIP := CopyIP(firstIP)
-	for i := 0; i < len(firstIP); i++ {
-		lastIP[i] = firstIP[i] | ^network.Mask[i] // use xor to create lastIP
+	if ipnet.IP.To4() != nil {
+		first = first.To4()
+		last = last.To4()
 	}
-
-	if network.IP.To4() != nil {
-		firstIP = firstIP.To4()
-		lastIP = lastIP.To4()
-	}
-
-	return firstIP, lastIP
+	return first, last
 }
 
-// func UsableNetRange(network *net.IPNet) (net.IP, net.IP) {
-// 	first, last := netRange
-// }
-
-// my function
+// IPLessThan returns true is IP a is smaller than IP b.
 func IPLessThan(a, b net.IP) bool {
 	a = a.To16()
 	b = b.To16()
@@ -164,10 +154,9 @@ func IPLessThan(a, b net.IP) bool {
 	return false
 }
 
-// myfunction
-// Given an IPNet return an IPNet where IP is the first address
+// IPNetNet takes an IPNet and returns an IPNet.IP is now the first address
 // on the network. E.g. when given 10.0.0.20/24, return 10.0.0.0/24.
-func IPNetNet(ipnet *net.IPNet) *net.IPNet {
+func IPNetNet(ipnet net.IPNet) *net.IPNet {
 	ip, _ := NetRange(ipnet)
 	return &net.IPNet{IP: ip, Mask: ipnet.Mask}
 }
--- nets_test.go
+++ nets_test.go
@@ -1,1 +1,112 @@
 package nets
+
+import (
+	"net"
+	"reflect"
+	"testing"
+)
+
+func TestIPInc(t *testing.T) {
+	tests := []struct {
+		name string
+		ip   net.IP
+		want net.IP
+	}{
+		{
+			name: "1.1.1.1",
+			ip:   net.IP{1, 1, 1, 1},
+			want: net.IP{1, 1, 1, 2},
+		},
+		{
+			name: "1.1.1.255",
+			ip:   net.IP{1, 1, 1, 255},
+			want: net.IP{1, 1, 2, 0},
+		},
+		{
+			name: "255.255.255.255",
+			ip:   net.IP{255, 255, 255, 255},
+			want: net.IP{0, 0, 0, 0},
+		},
+		{
+			name: "0000:0000:0000:0000:0000:0000:0000:0000",
+			ip:   net.ParseIP("0000:0000:0000:0000:0000:0000:0000:0000"),
+			want: net.ParseIP("0000:0000:0000:0000:0000:0000:0000:0001"),
+		},
+		{
+			name: "0000:0000:0000:0000:0000:0000:0000:ffff",
+			ip:   net.ParseIP("0000:0000:0000:0000:0000:0000:0000:ffff"),
+			want: net.ParseIP("0000:0000:0000:0000:0000:0000:0001:0000"),
+		},
+		{
+			name: "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff",
+			ip:   net.ParseIP("ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"),
+			want: net.ParseIP("0000:0000:0000:0000:0000:0000:0000:0000"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.ip
+			IPInc(got)
+			t.Log("got:", []byte(got))
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("IPAdd() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIPAdd(t *testing.T) {
+	type args struct {
+		ip     net.IP
+		offset int
+	}
+	tests := []struct {
+		name string
+		args args
+		want net.IP
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IPAdd(tt.args.ip, tt.args.offset); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("IPAdd() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+var resultIP net.IP
+
+func BenchmarkInc(b *testing.B) {
+	ip := net.IPv4(1, 1, 1, 1)
+	for i := 1; i <= b.N; i++ {
+		IPInc(ip)
+	}
+	b.Log("IP:", ip, "N:", b.N)
+}
+
+func BenchmarkAdd1(b *testing.B) {
+	ip := net.IPv4(1, 1, 1, 1)
+	for i := 1; i < b.N; i++ {
+		ip = IPAdd(ip, 1)
+	}
+	b.Log("IP:", ip, "N:", b.N)
+}
+
+func BenchmarkAdd(b *testing.B) {
+	ip := net.IPv4(1, 1, 1, 1)
+	ip = IPAdd(ip, b.N)
+	b.Log("IP:", ip, "N:", b.N)
+}
+
+func BenchmarkNetRange(b *testing.B) {
+	_, ipnet, _ := net.ParseCIDR("1.1.1.0/16")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		s, e := NetRange(*ipnet)
+		resultIP = s
+		resultIP = e
+	}
+}
